eb856da39e3ec5c4422dd91825daf696e56c756d
KilianCadiou <kiliancadiou@gmail.com>
2025-03-16 15:16:22 +0100

    Ajout pages

--- STR/pages/0 - proposition.py
+++ STR/pages/0 - proposition.py
@@ -12,81 +12,6 @@ voir = st.checkbox('Voir ma playlist.')
 if voir:
     st.dataframe(playlist)
 
-st.header("👇 Créer une liste de musique :")
-
-# AJOUTER UN GENRE ET UN ARTISTE
-
-genre = st.checkbox('Je veux ajouter un genre.')
-
-if genre:
-
-    ajout_genre_genre = st.text_input('Tapez le genre à ajouter :')
-
-    if ajout_genre_genre:
-        if ajout_genre_genre in playlist['Genre'].unique():
-            st.markdown('Le genre existe déjà.')
-        else:
-
-            international_artiste = st.toggle("L'artiste est-il français ?", value = False)
-            if international_artiste == False:
-                international = False
-            else:
-                international = True
-
-            ajout_artiste_genre = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
-            if ajout_artiste_genre:
-                if ajout_artiste_genre in playlist[playlist['Genre'] == ajout_genre_genre]['Artiste'].unique():
-                    st.markdown("L'artiste ou le groupe existe déjà.")
-                else:
-                    playlist.loc[len(playlist)] = [ajout_genre_genre, ajout_artiste_genre, international, 0]
-                    playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-                    playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
-
-# AJOUTER UN ARTISTE À UN GENRE EXISTANT
-
-artiste = st.checkbox('Je veux ajouter un artiste ou un groupe.')
-
-if artiste:
-
-    ajout_genre_artiste = st.text_input('Tapez le genre :')
-
-    if ajout_genre_artiste:
-        if ajout_genre_artiste not in playlist['Genre'].unique():
-            st.markdown("Le genre n'existe pas.")
-        else:
-
-            international_artiste = st.toggle("L'artiste est-il français ?", value = False)
-            if international_artiste == False:
-                international = False
-            else:
-                international = True
-
-            ajout_artiste_artiste = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
-
-            if ajout_artiste_artiste:
-                if ajout_artiste_artiste in playlist[playlist['Genre'] == ajout_genre_artiste]['Artiste'].unique():
-                    st.markdown("L'artiste ou le groupe existe déjà.")
-                else:
-                    playlist.loc[len(playlist)] = [ajout_genre_artiste, ajout_artiste_artiste, international, 0]
-                    playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-                    playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
-
-    st.dataframe(playlist)
-
-
-# SUPPRIMER UN ARTISTE
-
-supprimer = st.checkbox('Je veux supprimer un artiste ou un groupe.')
-
-if supprimer:
-    suppr_artiste = st.text_input("Tapez le nom de l'artiste ou du groupe :")
-    if suppr_artiste:
-        if suppr_artiste not in playlist['Artiste'].unique():
-            st.markdown("L'artiste ou le groupe n'est pas dans la playlist.")
-        else:
-            playlist = playlist[playlist['Artiste'] != suppr_artiste]
-            playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-            playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
 
 # PROPOSITION ARTISTE
 
@@ -113,12 +38,3 @@ if proposition:
     artiste_aleatoire = random.choice(liste_artistes)
     playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"] = playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"].apply(lambda x : x + 1)
     st.markdown(artiste_aleatoire)
-
-reboot = st.checkbox('Supprimer ma playlist.')
-
-if reboot:
-    reboot2 = st.checkbox('Êtes-vous sûr ?')
-    if reboot2:
-        playlist = pd.DataFrame(columns=['Genre', 'Artiste', 'Français', "Nombre d'écoutes"])
-        playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-        playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
--- STR/pages/1 - maj_playlist.py
+++ STR/pages/1 - maj_playlist.py
@@ -12,113 +12,92 @@ voir = st.checkbox('Voir ma playlist.')
 if voir:
     st.dataframe(playlist)
 
-st.header("👇 Créer une liste de musique :")
+actions = ['Je veux ...', 'Ajouter un élément', 'Modifier un élément', 'Supprimer un élément']
 
-# AJOUTER UN GENRE ET UN ARTISTE
+action = st.selectbox('Que souhaitez-vous faire ?', actions)
 
-genre = st.checkbox('Je veux ajouter un genre.')
+if action == 'Ajouter un élément':
 
-if genre:
+    # # AJOUTER UN GENRE ET UN ARTISTE
 
-    ajout_genre_genre = st.text_input('Tapez le genre à ajouter :')
+    # genre = st.checkbox('Je veux ajouter un genre.')
 
-    if ajout_genre_genre:
-        if ajout_genre_genre in playlist['Genre'].unique():
-            st.markdown('Le genre existe déjà.')
-        else:
+    # if genre:
 
-            international_artiste = st.toggle("L'artiste est-il français ?", value = False)
-            if international_artiste == False:
-                international = False
+    #     ajout_genre_genre = st.text_input('Tapez le genre à ajouter :')
+
+    #     if ajout_genre_genre:
+    #         if ajout_genre_genre in playlist['Genre'].unique():
+    #             st.markdown('Le genre existe déjà.')
+    #         else:
+
+    #             international_artiste = st.toggle("L'artiste est-il français ?", value = False)
+    #             if international_artiste == False:
+    #                 international = False
+    #             else:
+    #                 international = True
+
+    #             ajout_artiste_genre = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
+    #             if ajout_artiste_genre:
+    #                 if ajout_artiste_genre in playlist[playlist['Genre'] == ajout_genre_genre]['Artiste'].unique():
+    #                     st.markdown("L'artiste ou le groupe existe déjà.")
+    #                 else:
+    #                     playlist.loc[len(playlist)] = [ajout_genre_genre, ajout_artiste_genre, international, 0]
+    #                     playlist.to_csv('Musique/BD/playlist_a_jour.csv')
+    #                     playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
+
+    # AJOUTER UN ARTISTE À UN GENRE EXISTANT
+
+    artiste = st.checkbox('Je veux ajouter un artiste ou un groupe.')
+
+    if artiste:
+        
+        ajout_artiste_artiste = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
+
+        if ajout_artiste_artiste:
+            if ajout_artiste_artiste in playlist['Artiste'].unique():
+                st.markdown("L'artiste ou le groupe existe déjà.")
             else:
-                international = True
-
-            ajout_artiste_genre = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
-            if ajout_artiste_genre:
-                if ajout_artiste_genre in playlist[playlist['Genre'] == ajout_genre_genre]['Artiste'].unique():
-                    st.markdown("L'artiste ou le groupe existe déjà.")
+                international_artiste = st.toggle("L'artiste est-il français ?", value = False)
+                if international_artiste == False:
+                    international = False
                 else:
-                    playlist.loc[len(playlist)] = [ajout_genre_genre, ajout_artiste_genre, international, 0]
-                    playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-                    playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
+                    international = True
 
-# AJOUTER UN ARTISTE À UN GENRE EXISTANT
+                ajout_genre_artiste = st.text_input('Tapez le genre :')
 
-artiste = st.checkbox('Je veux ajouter un artiste ou un groupe.')
-
-if artiste:
-
-    ajout_genre_artiste = st.text_input('Tapez le genre :')
-
-    if ajout_genre_artiste:
-        if ajout_genre_artiste not in playlist['Genre'].unique():
-            st.markdown("Le genre n'existe pas.")
-        else:
-
-            international_artiste = st.toggle("L'artiste est-il français ?", value = False)
-            if international_artiste == False:
-                international = False
-            else:
-                international = True
-
-            ajout_artiste_artiste = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
-
-            if ajout_artiste_artiste:
-                if ajout_artiste_artiste in playlist[playlist['Genre'] == ajout_genre_artiste]['Artiste'].unique():
-                    st.markdown("L'artiste ou le groupe existe déjà.")
-                else:
+                if ajout_genre_artiste:
                     playlist.loc[len(playlist)] = [ajout_genre_artiste, ajout_artiste_artiste, international, 0]
                     playlist.to_csv('Musique/BD/playlist_a_jour.csv')
                     playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
 
-    st.dataframe(playlist)
 
+if action == 'Modifier un élément':
+    None
 
-# SUPPRIMER UN ARTISTE
+if action == 'Supprimer un élément':
 
-supprimer = st.checkbox('Je veux supprimer un artiste ou un groupe.')
+    # SUPPRIMER UN ARTISTE
 
-if supprimer:
-    suppr_artiste = st.text_input("Tapez le nom de l'artiste ou du groupe :")
-    if suppr_artiste:
-        if suppr_artiste not in playlist['Artiste'].unique():
-            st.markdown("L'artiste ou le groupe n'est pas dans la playlist.")
-        else:
-            playlist = playlist[playlist['Artiste'] != suppr_artiste]
+    supprimer = st.checkbox('Je veux supprimer un artiste ou un groupe.')
+
+    if supprimer:
+        suppr_artiste = st.text_input("Tapez le nom de l'artiste ou du groupe :")
+        if suppr_artiste:
+            if suppr_artiste not in playlist['Artiste'].unique():
+                st.markdown("L'artiste ou le groupe n'est pas dans la playlist.")
+            else:
+                playlist = playlist[playlist['Artiste'] != suppr_artiste]
+                playlist.to_csv('Musique/BD/playlist_a_jour.csv')
+                playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
+
+    # SUPPRIMER PLAYLIST
+
+    reboot = st.checkbox('Supprimer ma playlist.')
+
+    if reboot:
+        reboot2 = st.checkbox('Êtes-vous sûr ?')
+        if reboot2:
+            playlist = pd.DataFrame(columns=['Genre', 'Artiste', 'Français', "Nombre d'écoutes"])
             playlist.to_csv('Musique/BD/playlist_a_jour.csv')
             playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
-
-# PROPOSITION ARTISTE
-
-st.header('Choix Artiste')
-
-choix_genre = st.toggle('Je veux un genre précis.')
-
-if choix_genre:
-    playlist = playlist[playlist['Genre'] == choix_genre]
-
-choix_francais = st.toggle('Je veux de la musique française.')
-
-if choix_francais:
-    playlist = playlist[playlist['Français'] == choix_francais]
-
-choix_rare = st.toggle("Je veux un artiste que j'ai peu écouté.")
-if choix_rare:
-    playlist = playlist[playlist["Nombre d'écoutes"] == playlist["Nombre d'écoutes"].min()]
-
-proposition = st.checkbox('Propose moi un artiste')
-
-if proposition:
-    liste_artistes = list(playlist['Artiste'].unique())
-    artiste_aleatoire = random.choice(liste_artistes)
-    playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"] = playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"].apply(lambda x : x + 1)
-    st.markdown(artiste_aleatoire)
-
-reboot = st.checkbox('Supprimer ma playlist.')
-
-if reboot:
-    reboot2 = st.checkbox('Êtes-vous sûr ?')
-    if reboot2:
-        playlist = pd.DataFrame(columns=['Genre', 'Artiste', 'Français', "Nombre d'écoutes"])
-        playlist.to_csv('Musique/BD/playlist_a_jour.csv')
-        playlist = pd.read_csv('Musique/BD/playlist_a_jour.csv', index_col=0)
