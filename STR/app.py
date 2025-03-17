6db7873ed30b27e312f0baccd862834504e46e8c
KilianCadiou <kiliancadiou@gmail.com>
2025-03-17 17:43:19 +0100

    Update app.py

--- STR/app.py
+++ STR/app.py
@@ -48,12 +48,12 @@ st.logo("https://raw.githubusercontent.com/KilianCadiou/Culture_Aleatoire/refs/h
 
 
 
-config = toml.load(".streamlit/pages.toml")
+config = toml.load("STR/.streamlit/pages.toml")
 print(config)
 
 # nav = get_nav_from_toml("STR/.streamlit/pages.toml")
 
-nav = get_nav_from_toml(".streamlit/pages.toml")
+nav = get_nav_from_toml("STR/.streamlit/pages.toml")
 
 pg = st.navigation(nav)
 
