6129cfc2f366188e12c146326527a770d53e4245
KilianCadiou <kiliancadiou@gmail.com>
2025-03-17 15:14:55 +0100

    Concat des pages

--- STR/pages/2 - proposition_musique.py
+++ STR/pages/2 - proposition_musique.py
@@ -50,3 +50,74 @@ if proposition:
     
     if acceptation:
         playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"] = playlist[playlist['Artiste'] == artiste_aleatoire]["Nombre d'écoutes"].apply(lambda x : x + 1)
+
+st.markdown("<br><br>", unsafe_allow_html=True)
+
+st.header('Mettre à jour ma playlist.')
+
+actions = ['Je veux ...', 'Ajouter un élément', 'Modifier un élément', 'Supprimer un élément']
+
+action = st.selectbox('Que souhaitez-vous faire ?', actions)
+
+if action == 'Ajouter un élément':
+
+    # AJOUTER UN ARTISTE
+
+    artiste = st.checkbox('Je veux ajouter un artiste ou un groupe.')
+
+    if artiste:
+        
+        ajout_artiste_artiste = st.text_input("Tapez l'artiste ou le groupe à ajouter :")
+
+        if ajout_artiste_artiste:
+            if ajout_artiste_artiste in playlist['Artiste'].unique():
+                st.markdown("L'artiste ou le groupe existe déjà.")
+            else:
+                international_artiste = st.toggle("L'artiste est-il français ?", value = False)
+                if international_artiste == False:
+                    international = False
+                else:
+                    international = True
+
+                ajout_genre_artiste = st.text_input('Tapez le genre :')
+
+                if ajout_genre_artiste:
+                    playlist.loc[len(playlist)] = [ajout_genre_artiste, ajout_artiste_artiste, international, 0]
+                    playlist.to_csv('Culture_Aleatoire/BD/playlist_a_jour.csv')
+                    playlist = pd.read_csv('Culture_Aleatoire/BD/playlist_a_jour.csv', index_col=0)
+
+
+if action == 'Modifier un élément':
+    None
+
+if action == 'Supprimer un élément':
+
+    # SUPPRIMER UN ARTISTE
+
+    supprimer = st.checkbox('Je veux supprimer un artiste ou un groupe.')
+
+    if supprimer:
+        suppr_artiste = st.text_input("Tapez le nom de l'artiste ou du groupe :")
+        if suppr_artiste:
+            if suppr_artiste not in playlist['Artiste'].unique():
+                st.markdown("L'artiste ou le groupe n'est pas dans la playlist.")
+            else:
+                playlist = playlist[playlist['Artiste'] != suppr_artiste]
+                playlist.to_csv('Culture_Aleatoire/BD/playlist_a_jour.csv')
+                playlist = pd.read_csv('Culture_Aleatoire/BD/playlist_a_jour.csv', index_col=0)
+
+    # SUPPRIMER PLAYLIST
+
+    reboot = st.checkbox('Supprimer ma playlist.')
+
+    if reboot:
+        reboot2 = st.checkbox('Êtes-vous sûr ?')
+        if reboot2:
+            playlist = pd.DataFrame(columns=['Genre', 'Artiste', 'Français', "Nombre d'écoutes"])
+            playlist.to_csv('Culture_Aleatoire/BD/playlist_a_jour.csv')
+            playlist = pd.read_csv('Culture_Aleatoire/BD/playlist_a_jour.csv', index_col=0)
+
+voir = st.checkbox('Voir ma playlist.')
+
+if voir:
+    st.dataframe(playlist)
--- STR/pages/3 - proposition_livre.py
+++ STR/pages/3 - proposition_livre.py
@@ -31,3 +31,69 @@ if proposition:
         playlist = playlist[playlist['Titre'] != artiste_aleatoire]
         playlist.to_csv('Culture_Aleatoire/BD/livres_a_jour.csv')
         playlist = pd.read_csv('Culture_Aleatoire/BD/livres_a_jour.csv', index_col=0)
+
+
+st.markdown("<br><br>", unsafe_allow_html=True)
+st.header('Mettre à jour ma liste.')
+
+actions = ['Je veux ...', 'Ajouter un élément', 'Modifier un élément', 'Supprimer un élément']
+
+action = st.selectbox('Que souhaitez-vous faire ?', actions)
+
+if action == 'Ajouter un élément':
+
+    # AJOUTER UN FILM
+
+    artiste = st.checkbox('Je veux ajouter un livre.')
+
+    if artiste:
+        
+        ajout_artiste_artiste = st.text_input("Tapez le livre à ajouter :")
+
+        if ajout_artiste_artiste:
+            if ajout_artiste_artiste in playlist['Titre'].unique():
+                st.markdown("Le livre existe déjà.")
+            else:
+
+                ajout_genre_artiste = st.text_input('Tapez le genre :')
+
+                if ajout_genre_artiste:
+                    playlist.loc[len(playlist)] = [ajout_genre_artiste, ajout_artiste_artiste]
+                    playlist.to_csv('Culture_Aleatoire/BD/livres_a_jour.csv')
+                    playlist = pd.read_csv('Culture_Aleatoire/BD/livres_a_jour.csv', index_col=0)
+
+
+if action == 'Modifier un élément':
+    None
+
+if action == 'Supprimer un élément':
+
+    # SUPPRIMER UN ARTISTE
+
+    supprimer = st.checkbox('Je veux supprimer un livre.')
+
+    if supprimer:
+        suppr_artiste = st.text_input("Tapez le nom du livre :")
+        if suppr_artiste:
+            if suppr_artiste not in playlist['Titre'].unique():
+                st.markdown("Le livre n'est pas dans la liste.")
+            else:
+                playlist = playlist[playlist['Titre'] != suppr_artiste]
+                playlist.to_csv('Culture_Aleatoire/BD/livres_a_jour.csv')
+                playlist = pd.read_csv('Culture_Aleatoire/BD/livres_a_jour.csv', index_col=0)
+
+    # SUPPRIMER PLAYLIST
+
+    reboot = st.checkbox('Supprimer ma liste de livres.')
+
+    if reboot:
+        reboot2 = st.checkbox('Êtes-vous sûr ?')
+        if reboot2:
+            playlist = pd.DataFrame(columns=['Genre', 'Titre'])
+            playlist.to_csv('Culture_Aleatoire/BD/livres_a_jour.csv')
+            playlist = pd.read_csv('Culture_Aleatoire/BD/livres_a_jour.csv', index_col=0)
+
+voir = st.checkbox('Voir ma liste de livres.')
+
+if voir:
+    st.dataframe(playlist)
