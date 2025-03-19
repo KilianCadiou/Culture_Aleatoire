c0c361c146a92c22708057f18ed91374cc666410
KilianCadiou <kiliancadiou@gmail.com>
2025-03-19 17:25:56 +0100

    UP

--- STR/app.py
+++ STR/app.py
@@ -6,63 +6,6 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-# Authentification Google Drive
-def google_drive_auth():
-    gauth = GoogleAuth()
-    
-    # Authentification locale Streamlit Cloud (via les secrets)
-    gauth.LoadCredentialsFile(os.getenv('GOOGLE_DRIVE_CREDENTIALS'))
-    if gauth.credentials is None:
-        gauth.LocalWebserverAuth()  # Lancement du serveur local pour authentification
-    elif gauth.access_token_expired:
-        gauth.Refresh()
-    else:
-        gauth.Authorize()
-
-    gauth.SaveCredentialsFile(os.getenv('GOOGLE_DRIVE_CREDENTIALS'))
-    drive = GoogleDrive(gauth)
-    return drive
-
-# Télécharger un fichier CSV depuis Google Drive
-def download_csv(file_id, local_filename):
-    drive = google_drive_auth()
-    downloaded = drive.CreateFile({'id': file_id})
-    downloaded.GetContentFile(local_filename)
-    df = pd.read_csv(local_filename)
-    return df
-
-# Uploader un fichier CSV sur Google Drive
-def upload_csv(file_id, local_filename):
-    drive = google_drive_auth()
-    upload_file = drive.CreateFile({'id': file_id})
-    upload_file.SetContentFile(local_filename)
-    upload_file.Upload()
-
-# Fonction de gestion de l'interface Streamlit
-def main():
-    st.title("Gestion des CSV avec Google Drive")
-    
-    # ID du fichier CSV sur Google Drive (tu dois l'extraire de l'URL Google Drive)
-    file_id = 'TON_ID_CSV'
-    
-    # Télécharger le CSV
-    st.write("Téléchargement du fichier CSV...")
-    df = download_csv(file_id, 'films_a_jour.csv')
-    st.write(df.head())
-    
-    # Modifier le CSV
-    if st.button("Modifier le fichier CSV"):
-        df['Titre'] = df['Titre'].str.upper()  # Exemple de modification
-        df.to_csv('films_a_jour.csv', index=False)
-        st.write("Fichier modifié !")
-    
-    # Ré-uploader le fichier CSV modifié
-    if st.button("Uploader le fichier modifié sur Google Drive"):
-        upload_csv(file_id, 'films_a_jour.csv')
-        st.write("Fichier mis à jour sur Google Drive !")
-        
-if __name__ == "__main__":
-    main()
 
 custom_css = """
     <style>
